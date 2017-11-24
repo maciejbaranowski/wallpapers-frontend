a1b7af571f3e6fbdf877c6b0e4cf6c83e36d8291
Maciej Baranowski <maciekbaranowski@onet.pl>
2017-11-24 19:51:42 +0100

    Dataprovider aligned to PHP API

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -1,26 +1,25 @@
 import axios from "axios";
 
 class DataProvider {
-  static getApiUrl = () => "https://tapety-backend.herokuapp.com/api/"; //production
-  //static getApiUrl = () => "http://127.0.0.1:5000/api/"; //local
+  static getApiUrl = () => "http://tapetycytaty.pl/api/";
   static getImagesHost = () => "http://tapetycytaty.pl/";
 
   static getCategoriesList() {
-    return axios.get(this.getApiUrl() + "getCategories", {
+    return axios.get(this.getApiUrl() + "getCategories.php", {
       crossDomain: true,
       params: {}
     });
   }
 
   static getAuthorsList() {
-    return axios.get(this.getApiUrl() + "getAuthors", {
+    return axios.get(this.getApiUrl() + "getAuthors.php", {
       crossDomain: true,
       params: {}
     });
   }
 
   static getWallpaper(index) {
-    return axios.get(this.getApiUrl() + "getImage", {
+    return axios.get(this.getApiUrl() + "getImage.php", {
       crossDomain: true,
       params: {
         id: index
@@ -29,7 +28,7 @@ class DataProvider {
   }
 
   static getWallpapers(category, from, number, searchPhrase, author) {
-    return axios.get(this.getApiUrl() + "getImages", {
+    return axios.get(this.getApiUrl() + "getImages.php", {
       crossDomain: true,
       params: {
         firstIdx: from,
@@ -43,10 +42,12 @@ class DataProvider {
 
   static postVote(index, vote) {
     return axios.post(
-      this.getApiUrl() + "postVote",
+      this.getApiUrl() + "postVote.php",
       {
-        index: index,
-        vote: vote
+        data: {
+          index: index,
+          vote: vote
+        }
       },
       {
         crossDomain: true
@@ -56,10 +57,12 @@ class DataProvider {
 
   static sendMessage(email, content) {
     return axios.post(
-      this.getApiUrl() + "sendMessage",
+      this.getApiUrl() + "sendMessage.php",
       {
-        sender: email,
-        content: content
+        data: {
+          sender: email,
+          content: content
+        }
       },
       {
         crossDomain: true

b07da9a72f0f94f558cdb3e35a3b014f09add0f7
Maciej Baranowski <maciekbaranowski@onet.pl>
2019-04-09 13:51:36 +0200

    Loader CSS fix, GA and canonicalSettter moved to _app

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ import {LoadingPane} from "../components/LoadingPane";
 import App, {Container} from "next/app"
 import {withRouter} from "next/router"
 
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-109847829-2");
 
 class CustomApp extends App {
   static async getInitialProps({Component, ctx}) {
@@ -17,6 +19,31 @@ class CustomApp extends App {
 
     return {pageProps};
   }
+
+  reportAnalytics = () => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    return null;
+  };
+  
+  fixCanonical = () => {
+    let oldLinks = document.getElementsByTagName("link");
+    Array
+      .from(oldLinks)
+      .forEach(e => {
+        if (e.rel === "canonical") {
+          e
+            .parentNode
+            .removeChild(e);
+        }
+      });
+    let linkTag = document.createElement("link");
+    linkTag.rel = "canonical";
+    linkTag.href = window.location;
+    document
+      .getElementsByTagName("head")[0]
+      .appendChild(linkTag);
+    return null;
+  };
 
   constructor(props) {
     super(props);
@@ -39,7 +66,9 @@ class CustomApp extends App {
       .events
       .on('routeChangeError', () => {
         this.setState({loading: false})
-      })
+      });
+    this.fixCanonical();
+    this.reportAnalytics();
   }
   render() {
     const { Component, pageProps } = this.props;
--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import ReactGA from "react-ga";
-ReactGA.initialize("UA-109847829-2");
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-/*
-  reportAnalytics = () => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    return null;
-  };
-  
-  fixCanonical = () => {
-    let oldLinks = document.getElementsByTagName("link");
-    Array
-      .from(oldLinks)
-      .forEach(e => {
-        if (e.rel === "canonical") {
-          e
-            .parentNode
-            .removeChild(e);
-        }
-      });
-    let linkTag = document.createElement("link");
-    linkTag.rel = "canonical";
-    linkTag.href = window.location;
-    document
-      .getElementsByTagName("head")[0]
-      .appendChild(linkTag);
-    return null;
-  };
-*/
+
   render() {
     return (
       <Html>

dfc52c14c534372c006bc293190f56ed519ce34b
Maciej Baranowski <maciekbaranowski@onet.pl>
2019-04-09 13:24:19 +0200

    API URLs updated

--- components/DataProvider.js
+++ components/DataProvider.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 class DataProvider {
-  static getApiUrl = () => "https://tapetycytaty.pl/api/";
-  static getImagesHost = () => "https://tapetycytaty.pl/";
+  static getApiUrl = () => "https://api.tapetycytaty.pl/api/";
+  static getImagesHost = () => "https://api.tapetycytaty.pl/";
 
   static getCategoriesList() {
     return axios.get(this.getApiUrl() + "getCategories.php", {

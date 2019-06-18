0f90e42993bd00cb1e5811c16c76dae8bcbc9ffc
Maciej Baranowski <maciekbaranowski@onet.pl>
2019-06-18 20:45:48 +0200

    Google Adsense fix (from body to head)

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script>
+                {`(adsbygoogle = window.adsbygoogle || []).push({
+                      google_ad_client: "ca-pub-2855266633887827",
+                      enable_page_level_ads: true
+                });`}
+          </script>
           <link href="/static/bootstrap.min.css" rel="stylesheet"/>
           <link href="/static/index.css" rel="stylesheet"/>
           <link href="/static/loader.css" rel="stylesheet"/>
@@ -17,13 +24,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script>
-              {`(adsbygoogle = window.adsbygoogle || []).push({
-                    google_ad_client: "ca-pub-2855266633887827",
-                    enable_page_level_ads: true
-              });`}
-          </script>
+
         </body>
       </Html>
     );

7bbccf40bf6fcef4aae53f9df9187cd544058e9b
Maciej Baranowski <maciekbaranowski@onet.pl>
2019-09-14 15:06:21 +0200

    Expanded tests for WallpaperList component

--- components/WallpaperList.test.js
+++ components/WallpaperList.test.js
@@ -4,6 +4,19 @@ import { LoadingPane } from "./LoadingPane";
 import Wallpaper from "./Wallpaper";
 
 describe("Wallpaper List test", () => {
+  const wallpapersData = [
+    {
+      quote:"Hello World",
+      author:"John Smith",
+      categoryName:"O niczym"
+    },
+    {
+      quote:"Another one",
+      author:"Adam Smith",
+      categoryName:"O czyms"
+    } 
+  ];
+
   it("Should render loading pane when data not fetched", () => {
       const component = renderer.create(<WallpaperList />);
 
@@ -14,16 +27,7 @@ describe("Wallpaper List test", () => {
         page={0}
         wallpapersNumber={2}
         wallpapersPerPage={10}
-        wallpapersList={[
-          {
-            quote:"Hello World",
-            author:"John Smith"
-          },
-          {
-            quote:"Another one",
-            author:"Adam Smith"
-          }
-        ]}
+        wallpapersList={wallpapersData}
         dataFetched={true}
       />);
 
@@ -31,5 +35,19 @@ describe("Wallpaper List test", () => {
       const wallpapers = component.root.findAllByType(Wallpaper); 
       expect(wallpapers.length).toEqual(2);
       expect(wallpapers[0].findByType("h4").children[0]).toEqual("Hello World");
+      expect(wallpapers[1].findByType("h6").children[1]).toEqual("O czyms");
+  });
+
+  it("Should render next button on main page if more wallpapers to come", () => {
+    const component = renderer.create(<WallpaperList 
+      page={0}
+      wallpapersNumber={3}
+      wallpapersPerPage={2}
+      wallpapersList={wallpapersData}
+      dataFetched={true}
+    />);
+    const wallpapers = component.root.findAllByType(Wallpaper); 
+    expect(wallpapers.length).toEqual(2);
+    expect(component.root.findByProps({href: "/?category=&page=1"}));
   });
 });
